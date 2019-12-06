9b2ba96fd258a40fdc4f908f4a95a7833ed1d305
Reece Pierson <piersonreece@gmail.com>
2019-12-06 12:56:47 -0600

    wrote tests. abandoned search form

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
-import { PlayerList } from './components/PlayerList';
+import PlayerList from './components/PlayerList';
 
-class App extends React.Component {
-
-  state = {
-    players: []
-  }
-
-  componentDidMount(){
-    console.log('cDM');
-
-    axios
-      .get('http://localhost:5000/api/players')
-      .then((res) => {
-        console.log(res.data)
-        this.setState({players: res.data})
-        console.log('Players state', this.state.players)
-      })
-
-  }
+const App = () => {
   
-  render() {
     return (
       <div className="App">
-        <PlayerList players={this.state.players}/>
+        <PlayerList/>
       </div>
     )
-  }
+  
   
 }
 
--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import {PlayerCard} from './PlayerCard'
+import axios from 'axios';
 
-export const PlayerList = props => {
-    return (
-        <div>
-            {props.players.map(player =>(
-                <PlayerCard key={player.id} player={player}/>
+class PlayerList extends React.Component  {
+    state = {
+        players: []
+      }
+    
+    componentDidMount(){
+        
+        axios
+          .get('http://localhost:5000/api/players')
+          .then((res) => {
+            this.setState({players: res.data})
+           
+          })
+    
+    }
+
+    render(){
+        return (
+        <div data-testId='playerList'>
+            {this.state.players.map(player =>(
+                <PlayerCard key={player.id} player={player} data-testId='player'/>
             ))}
         </div>
     )
+    }
+    
 }
+
+export default PlayerList
